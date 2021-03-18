c6965afb8c8a40dbf9bd6e3fa2b5e048aca7e22f
Péter Tamás Weber <peter.weber@stud.uni-corvinus.hu>
2021-03-18 12:50:50 +0100

    Félkész

---
+++ HajosTeszt/HajosTeszt/wwwroot/jsgyak.js
@@ -0,0 +1,28 @@
+﻿var faktoriálisR = (n) => {
+    if (n === 0 || n === 1) {
+        return 1;
+    } else {
+        return n * faktoriálisR(n - 1)
+    }
+    
+}
+
+
+
+window.onload = function () {
+    console.log("start")
+
+    let hova = document.getElementById("pascal");
+    for (var s = 0; s < 10; s++) {
+        let sor = document.createElement("div");
+        sor.classList.add("sor")
+        hova.appendChild(sor);
+
+        for (var o = 0; o < 10; o++) {
+            let szám = document.createElement("div");
+            szám.classList.add("elem")
+            szám.innerHTML = `${faktoriálisR(s)}`
+            sor.appendChild(szám);
+        }
+    }
+}
